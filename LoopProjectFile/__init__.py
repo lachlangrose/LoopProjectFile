039c9f831dee2821f9f1e2945a6508b230b71d92
Roy Thomson <Roy.Thomson@monash.edu>
2021-11-23 12:35:39 +1100

    Update events to separate group from supergroup, add angle and type of relationship to EventRelationship structure

--- LoopProjectFile/__init__.py
+++ LoopProjectFile/__init__.py
@@ -2,7 +2,7 @@ from .LoopProjectFile import CreateBasic, Get, Set, OpenProjectFile, CheckFileVa
         faultEventType, foldEventType, discontinuityEventType, foliationEventType, \
         faultObservationType, foldObservationType, foliationObservationType, discontinuityObservationType, \
         stratigraphicLayerType, stratigraphicObservationType, contactObservationType, \
-        eventRelationshipType, ConvertDataFrame
+        eventRelationshipType, ConvertDataFrame, EventType, EventRelationshipType
 from .Permutations import Event, perm, ApproxPerm, CalcPermutation, checkBrokenRules, checkBrokenEventRules
 from .LoopProjectFileUtils import ToCsv, FromCsv, ElementToCsv, ElementFromCsv
 from .Version import LoopVersion
