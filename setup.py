c344ebbe904a25c3eb3473e09db741b273400974
Lachlan Grose <lachlan.grose@monash.edu>
2024-03-11 11:59:21 +1100

    fix: migrating from setup.py to pyproject.toml (#5)

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 import os
 
 package_root = os.path.abspath(os.path.dirname(__file__))
@@ -8,27 +8,4 @@ with open(os.path.join(package_root, "LoopProjectFile/Version.py")) as fp:
     exec(fp.read(), version)
 version = version["__version__"]
 
-setup(
-    name="LoopProjectFile",
-    install_requires=["netCDF4>=1.5.4", "numpy", "pandas"],
-    description="Open source structural geology data storage for Loop Projects",
-    author="Roy Thomson",
-    author_email="roy.thomson@monash.edu",
-    url="https://github.com/Loop3D/LoopProjectFile",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Other Audience",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
-        "License :: Free for non-commercial use",
-        "Natural Language :: English",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Topic :: Multimedia :: Graphics :: 3D Modeling",
-        "Topic :: Scientific/Engineering",
-        "Topic :: Scientific/Engineering :: GIS",
-    ],
-    version=version,
-    packages=find_packages(),
-)
+setup()
